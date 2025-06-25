8546770bd3a6e14ae38a70363e1dc24398989f18
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-06-25 10:07:57 +0200

    completed exercise 1.6

--- part-1/unicafe/src/App.tsx
+++ part-1/unicafe/src/App.tsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+type FeedbackType = "good" | "neutral" | "bad";
 
-  return (
-    <div>
-      code here
-    </div>
-  )
+interface FeedbackButtonProps {
+  text: FeedbackType;
+  onClick: () => void;
 }
 
-export default App
+const FeedbackButton = ({ text, onClick }: FeedbackButtonProps) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+interface FeedbackDisplayProps {
+  text: FeedbackType;
+  count: number;
+}
+
+const FeedbackDisplay = ({ text, count }: FeedbackDisplayProps) => {
+  return <p>{text}: {count}</p>;
+};
+
+
+
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <FeedbackButton text="good" onClick={() => setGood(good + 1)} />
+      <FeedbackButton text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <FeedbackButton text="bad" onClick={() => setBad(bad + 1)} />
+
+      <h1>Statistics</h1>
+      <FeedbackDisplay text="good" count={good} />
+      <FeedbackDisplay text="neutral" count={neutral} />
+      <FeedbackDisplay text="bad" count={bad} />
+    </>
+  );
+};
+
+export default App;
