b3ed7f768b58fc8fd0a87c40a863905a1e30cd11
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-06-30 16:30:10 +0200

    added a little readme in app.tsx explaining my design choices.

--- part-2/countries/src/App.tsx
+++ part-2/countries/src/App.tsx
@@ -5,6 +5,24 @@ import countriesService from './services/countries';
 import buildSearchIndex from './services/buildSearchIndex';
 import type { Country, SearchIndex } from './services/buildSearchIndex';
 
+/* 
+Read me
+In This project I've opted for te following structure, I'm not sure if its the best for this application but I'll try to argue my reasons:
+1.  I've fetched all the country info and extracted just the name and official name and put them in a search index to always reference to common name of the country.
+    You could add alt spellings or translation into this as well, but based on the simple nature of the search engine this resulted in weird behavior sometimes.
+
+2.  The user writes in the SearchBar, based on the created search index a list of matches roles out. I added support for the 'sudan' cases by first looking for perfect matches and if not found for partial matches.
+3.  These are handled as instructed; if this list is larger than 1 and smaller than 10 show the list of results and a button attached which will directly bring you to the information page of the corresponding country
+    In case of a single match, the information page is loaded automatically.
+
+4.  Here come the design choices I'm not quite sure of: So I chose to fetch the countries data from the api just when the country info should be loaded.
+    This as to not create to not create to many api calls, or store more data than necessary. The latter is a habit I adopted from computational science in python though in this project it don't think it would matter much.
+    I adopted the same method for the weather information, in this case it is smart because for the weather api you can only fetch one city at a time, and you'd need to refresh the current weather every 15 minutes
+    This would result in a lot of API calls, and since this API will start costing money after 1000 calls. I thought this'd be the best way of handling it.
+    Looking back I think it would've been best to store the countries data ahead of time and just call the weather api on render, but I don't feel like fixing it since I won't learn much from it.
+*/
+
+
 function App() {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchIndex, setSearchIndex] = useState<SearchIndex>({});
