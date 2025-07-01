f6f8268ff57116fe9ebc8fcd97f0265fc91a6a87
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-01 17:28:57 +0200

    completed exercise 3.3

--- part-3/phonebook (server)/src/index.ts
+++ part-3/phonebook (server)/src/index.ts
@@ -1,15 +1,72 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-// Middleware to parse JSON
 app.use(express.json());
+
+interface Contact {
+  id: string,
+  name: string,
+  number: string
+}
+
+const phonebookEntries = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
 });
+
+app.get('/info', (req: Request, res: Response) => {
+  const currentTime = new Date()
+  const totalEntries = phonebookEntries.length
+
+  res.send(`
+    <div>
+      <p>Phonebook has info for ${totalEntries} people</p>
+      <p>${currentTime}</p>
+    `)
+})
+
+app.get('/api/persons', (req: Request, res: Response) => {
+  res.json(phonebookEntries)
+})
+
+app.get('/api/persons/:id', (req: Request, res: Response) => {
+  const id = req.params.id
+  const person = phonebookEntries.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.statusMessage = "There is no person that matches this id"
+    res.status(404).end()
+  }
+})
+
+
 
 // Start the server
 app.listen(PORT, () => {
