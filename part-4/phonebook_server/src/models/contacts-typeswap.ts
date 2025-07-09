473f47f1f955bcc54c2c285eede33b1e76c463a8
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-09 18:05:40 +0200

    fixed type import issue

--- part-4/phonebook_server/src/models/contacts-typeswap.ts
+++ part-4/phonebook_server/src/models/contacts-typeswap.ts
@@ -1,8 +1,8 @@
-import type { Contact } from '../index'
+import type { Contact } from '../types/contact-types'
 import type { IContact } from './contactDB'
 
-const mapIContactToContact = (inObj: IContact): ContactDB => {
-  const outObj: ContactDB = {
+const mapIContactToContact = (inObj: IContact): Contact => {
+  const outObj: Contact = {
     id: inObj.id,
     name: inObj.name,
     number: inObj.number,
