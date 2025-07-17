7e76607d7dc0bca8414f0ac81e942dcfe3e63f8e
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-17 15:00:51 +0200

    added test mode

--- part-4/blog-list_backend/src/utils/config.ts
+++ part-4/blog-list_backend/src/utils/config.ts
@@ -4,7 +4,7 @@ import logger from './logger'
 dotenv.config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 
 if (!MONGODB_URI) {
   logger.error('MONGODB_URI is not defined in the configuration')
