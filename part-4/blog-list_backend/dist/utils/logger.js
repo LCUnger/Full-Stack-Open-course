1c43ec71484fc787b827d5dd36b4f7d557544633
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-18 14:30:59 +0200

    tested api

--- part-4/blog-list_backend/dist/utils/logger.js
+++ part-4/blog-list_backend/dist/utils/logger.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const info = (...params) => {
-    console.log(...params);
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(...params);
+    }
 };
 const error = (...params) => {
-    console.error(...params);
+    if (process.env.NODE_ENV !== 'test') {
+        console.error(...params);
+    }
 };
 exports.default = { info, error };
--- part-4/blog-list_backend/src/tests/blog_api.test.ts
+++ part-4/blog-list_backend/src/tests/blog_api.test.ts
@@ -4,71 +4,45 @@ import mongoose from 'mongoose'
 import supertest from 'supertest'
 import app from '../app'
 import Blog from '../models/blog_model'
+import helper from './blog_api_helper'
+
+
 import type { DbBlogType } from '../types/blog'
 
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  },
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  }
-]
-
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  for (const blog of initialBlogs) {
+  for (const blog of helper.initialBlogs) {
     const blogObject = new Blog(blog)
     await blogObject.save()
   }
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    _id: "3a822bc61b54b897534d17fc",
+    title: "Coffeee",
+    author: "James Hoffman",
+    url: "url here",
+    likes: 1,
+    __v: 0
+  }
 
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map((e:any) => e.content)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length+1)
+})
 
 
 test('blogs are returned as json', async () => {
@@ -76,6 +50,8 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('blog without ')
 
 test('a specific blog is within the returned notes', async () => {
   const response = await api.get('/api/blogs')
@@ -87,10 +63,11 @@ test('a specific blog is within the returned notes', async () => {
 test('all notes are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, initialBlogs.length)
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 
 after(async () => {
-  await mongoose.connection.close
+  await mongoose.connection.close()
+  console.log('connection closed')
 })
