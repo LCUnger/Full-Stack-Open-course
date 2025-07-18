a22a114bb1d7beb85336a551a238e93a9a609d70
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-18 13:52:15 +0200

    changed async functioning

--- part-4/blog-list_backend/src/controllers/blogs_router.ts
+++ part-4/blog-list_backend/src/controllers/blogs_router.ts
@@ -6,13 +6,12 @@ import Blog from '../models/blog_model'
 
 const blogsRouter = express.Router()
 
-blogsRouter.get('/', (request, response: Response<DbBlogType[]>) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response: Response<DbBlogType[]>) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request: Request<{}, {}, BlogType>, response: Response, next: NextFunction) => {
+blogsRouter.post('/', async (request: Request<{}, {}, BlogType>, response: Response, next: NextFunction) => {
   const blog = new Blog(request.body)
 
   blog.save()
