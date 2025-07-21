99a36e7ddaf3a8298756d8f3bda8d1b5770f699f
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-21 10:18:22 +0200

    added support for user in blog schema and type

--- part-4/blog-list_backend/src/types/blog_types.ts
+++ part-4/blog-list_backend/src/types/blog_types.ts
@@ -1,9 +1,12 @@
+import mongoose from "mongoose";
+
 export interface BlogType {
   title: string;
   author: string;
   url: string;
   likes: number;
   id?: string;
+  user: mongoose.Types.ObjectId
 }
 
 export interface DbBlogType extends BlogType, Document {}
