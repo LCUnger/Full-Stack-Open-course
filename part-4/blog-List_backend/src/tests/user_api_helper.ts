93865d7253e84dee73416ff7d470c39385a6117c
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-21 10:30:58 +0200

    added pre-determined _id's to initial users

--- part-4/blog-List_backend/src/tests/user_api_helper.ts
+++ part-4/blog-List_backend/src/tests/user_api_helper.ts
@@ -2,31 +2,36 @@ import bcrypt from 'bcrypt'
 
 const initialUsersData = [
   {
+    _id: "687cef71832967f2c38f6b4f",
     username: "johndoe",
     name: "John Doe",
     password: "password123"
   },
   {
+    _id: "a3f9d8e7b2c947e1a5d6f4c3",
     username: "janedoe",
     name: "Jane Doe", 
     password: "mypassword"
   },
   {
+    _id: "c4e7f8a9b3d6e2f1a5c9d8b7",
     username: "alexsmith",
     name: "Alex Smith",
     password: "secret456"
   },
   {
+    _id: "d9b8c7a6f5e4b3c2a1f7e8d9",
     username: "sarahwilson",
     name: "Sarah Wilson",
     password: "qwerty789"
   },
   {
+    _id: "e8f7d6c5b4a3e2f1a9c8b7d6",
     username: "mikejohnson",
     name: "Mike Johnson",
     password: "admin2024"
   }
-]
+];
 
 const getInitialUsers = async () => {
   const saltRounds = 10
