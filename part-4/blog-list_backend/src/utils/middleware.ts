4a9d0ac9cf9020c5d05be97b19069e650c881757
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-22 11:58:05 +0200

    added error cases

--- part-4/blog-list_backend/src/utils/middleware.ts
+++ part-4/blog-list_backend/src/utils/middleware.ts
@@ -7,8 +7,13 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   logger.error(error)
   
   if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
-
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicated key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   next(error)
