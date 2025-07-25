80cb4e6cbce58590e0a2d2d184599824823173e8
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-25 10:39:29 +0200

    removed copied over folders of part-3 since they ended up being unnecessary

--- part-4/phonebook_server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './app'
-
-import config from './utils/config'
-import logger from './utils/logger'
-
-// Start the server
-app.listen(config.PORT, () => {
-  logger.info(`Server is running on p0rt ${config.PORT}`)
-})
--- part-4/phonebook_server/src/types/contact-types.ts
+++
@@ -1,5 +0,0 @@
-export interface Contact {
-  id: string
-  name: string
-  number: string
-}
