ffd7857327d07b5b8c6cf42b2d2ce896c5edce80
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-27 00:09:19 +0200

    created inlog field component

--- part-5/bloglist-frontend/src/components/InlogField.tsx
+++ part-5/bloglist-frontend/src/components/InlogField.tsx
@@ -1,12 +1,36 @@
 import { useState } from "react"
+import loginService from "../services/login.service"
+import { useAuth } from "../contexts/useAuthContext"
+
 
 const InlogField = () => {
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
 
+  const { login, user, logout } = useAuth()
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+    const userData = await loginService.login({ username, password });
+    login(userData)
+    } catch (error) {
+      console.error('Login failed:', error)
+    }
+  }
+
+  if (user) {
+    return (
+    <div>
+      Logged in as: {user.username}
+      <button onClick={logout}>logout</button>
+    </div>
+    )
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <table>
           <tbody>
             <tr>
@@ -29,6 +53,11 @@ const InlogField = () => {
                   onChange={event => setPassword(event.target.value)}
                   name="password"
                 />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button type='submit'>login</button>
               </td>
             </tr>
           </tbody>
