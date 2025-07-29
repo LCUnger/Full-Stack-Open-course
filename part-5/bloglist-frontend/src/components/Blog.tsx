49b2410b15c2049565e34b0d5df24f906396736c
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-07-29 11:51:05 +0200

    liking feature to blog

--- part-5/bloglist-frontend/src/components/Blog.tsx
+++ part-5/bloglist-frontend/src/components/Blog.tsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 import type { BlogType } from "../types/blog.types";
 import styles from "../styles/Blog.module.css";
+import blogsService from "../services/blogs.service";
 
 const Blog = ({blog}: {blog: BlogType}) => {
   const [displayDetails, setDisplayDetails] = useState(false)
+  const [liked, setLiked] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
 
   const toggleDisplayDetails = () => {
     setDisplayDetails(!displayDetails)
+  }
+
+  const handleLike = async () => {    
+    try {
+      
+      const newLiked = !liked
+      const newLikesCount = !liked ? likes + 1 : likes - 1
+
+      // Optimistic updates - update UI immediately
+      setLiked(newLiked)
+      setLikes(newLikesCount)
+
+      const updatedBlog = await blogsService.update({...blog, likes: newLikesCount})
+      setLikes(updatedBlog.likes)
+      console.log('current likes', likes)
+
+    } catch (error) {
+      // Rollback on error
+      setLiked(liked)
+      setLikes(likes)
+      console.error('Failed to update like: ', error)
+    }
   }
 
   if (displayDetails) {
@@ -19,8 +44,16 @@ const Blog = ({blog}: {blog: BlogType}) => {
         <div className={styles.blogDetails}>
           <div>URL: <a href={blog.url}>{blog.url}</a></div>
           <div>
-            Likes: {blog.likes}
-            <button>like</button>
+            Likes: {likes}
+            <button 
+              onClick={handleLike} 
+              style={{ 
+                backgroundColor: liked ? '#dc3545' : '#28a745',
+                color: 'white'
+              }}
+            >
+              {liked ? 'Unlike' : 'Like'}
+            </button>
           </div>
           <div>Added by: {blog.user?.name}</div>
         </div>
