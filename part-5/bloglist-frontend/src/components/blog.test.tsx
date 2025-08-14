21db0d8b01a51f6740ce60be72040cdbd02d61c5
Luuk Unger <l.c.unger@student.tudelft.nl>
2025-08-14 13:09:56 +0200

    completed exercise 5.14

--- part-5/bloglist-frontend/src/components/blog.test.tsx
+++ part-5/bloglist-frontend/src/components/blog.test.tsx
@@ -1,27 +1,32 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Blog from './Blog'
 import type { BlogType } from '../types/blog.types'
 import NotificationProvider from '../contexts/NotificationContext'
 import AuthProvider from '../contexts/AuthContext'
 
-test('renders content', () => {
-  const blog: BlogType = {
-    title: "test blog 1",
-    author: "test author",
-    url: "test.example.com",
-    likes: 7,
-    id: "69",
-    user: {
-      _id: "345678",
-      username: "Vitest_Tester",
-      name: "Vitest"
-    }
-  }
 
+const testBlog: BlogType = {
+  title: "test blog 1",
+  author: "test author",
+  url: "test.example.com",
+  likes: 7027346,
+  id: "69",
+  user: {
+    _id: "345678",
+    username: "Vitest_Tester",
+    name: "Vitest"
+  }
+}
+
+
+
+test('renders content', () => {
   const { container } = render(
     <AuthProvider>
       <NotificationProvider>
-        <Blog blog={blog} onBlogRemoved={() => {console.log('remove blog...temp')}} />
+        <Blog blog={testBlog} onBlogRemoved={() => {console.log('remove blog...temp')}} />
       </NotificationProvider>
     </AuthProvider>
   )
@@ -30,7 +35,35 @@ test('renders content', () => {
 
   expect(blogElement).toBeTruthy()
 
-  expect(blogElement?.textContent).toContain(blog.title)
+  expect(blogElement?.textContent).toContain(testBlog.title)
+  expect(blogElement?.textContent).toContain(testBlog.author)
+  expect(blogElement?.textContent).not.toContain(testBlog.url)
+  expect(blogElement?.textContent).not.toContain(testBlog.likes)
+})
 
-  screen.debug()
+test('renders hidden content after expanding', async () => {
+  const user = userEvent.setup()
+
+  const { container } = render(
+    <AuthProvider>
+      <NotificationProvider>
+        <Blog blog={testBlog} onBlogRemoved={() => {console.log('remove blog...temp')}} />
+      </NotificationProvider>
+    </AuthProvider>
+  )
+
+  const blogElement = container.querySelector('.blog')
+
+  const viewButton = screen.getByRole('button', { name: 'view' })
+  expect(viewButton).toBeInTheDocument()
+
+  await user.click(viewButton)
+
+    expect(blogElement).toBeTruthy()
+
+  expect(blogElement?.textContent).toContain(testBlog.title)
+  expect(blogElement?.textContent).toContain(testBlog.author)
+  expect(blogElement?.textContent).toContain(testBlog.url)
+  expect(blogElement?.textContent).toContain(testBlog.likes)
+
 })
